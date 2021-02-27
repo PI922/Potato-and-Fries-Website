0eec24358150dd0e076255d506cdaf4cc43cac79
gary yang <garyyang1688@gmail.com>
2021-02-27 01:57:42 +0000

    A lot

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+from os import system, name
+def clear():
+    if name == 'nt': 
+       _ = system('cls')
+
+    else:
+       _ = system('clear')
+
+door = random.randint (1,3)
+dif = input("Welcome to the Potato and Fries Help Center. What would you like to do?\nP.S. Enter 1-3 to do what is on the list. \n1. File a complaint\n2. File an work request\n3. Report a bug \n\n ")
+
+if dif == "1":
+    st = input("\nTry entering this link so you can hopefully resolve your error.\n https://docs.google.com/forms/d/e/1FAIpQLScixeMDROeSKIkyuW9uYaSy57p0qcZRtptrgRy8QVqZvhvJHw/viewform")
+
+if dif == "2":
+    st = input
